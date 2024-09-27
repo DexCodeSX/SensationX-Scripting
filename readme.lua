d638514b3213791ae356edd6a07d33d88f863fc5
DexCodeSX <windowbisam@gmail.com>
2024-09-27 12:18:26 +0500

    Create readme.lua

---
+++ readme.lua
@@ -0,0 +1,10 @@
+-- SensationX - README
+-- Upcoming Release Test Script
+
+print("Welcome to the SensationX exploit.")
+print("Test coming soon. Stay tuned for the official release script.")
+
+-- GitHub repository for more information:
+print("Check out the official GitHub for updates: https://github.com/DexCodeSX/Bisam-Aboutme/tree/main")
+
+-- End of README
